6e825bd8d7e632c3cc42845dff3a28cbde295f05
Dor Bar <dor4231@gmail.com>
2018-08-07 23:35:48 +0300

    feat: Create externalfrom App BookShelve component

---
+++ src/BookShelve.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+
+class BookShelve extends Component {
+
+    render() {
+        console.log(this.props.shelves)
+    }
+}
