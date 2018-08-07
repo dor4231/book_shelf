af4cb4ab340b9b48e226db323fa70eecdc097b68
Dor Bar <dor4231@gmail.com>
2018-08-08 00:11:41 +0300

    feat: BookShelve is now generated dynamicly form the app state

--- src/BookShelve.js
+++ src/BookShelve.js
@@ -3,8 +3,55 @@ import { Link } from 'react-router-dom'
 
 
 class BookShelve extends Component {
+    
 
     render() {
-        console.log(this.props.shelves)
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                <h1>MyReads - From Comp</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {this.props.shelves.map((shelve) => (
+                            <div className="bookshelf" key={shelve.name}>
+                                <h2 className="bookshelf-title">{shelve.name}</h2>
+                                <div className="bookshelf-books">
+                                <ol className="books-grid">    
+                                    {shelve.books.map((book) => (
+                                        <li key={book.title}>
+                                            <div className="book">
+                                                <div className="book-top">
+                                                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${book.cover}")` }}></div>
+                                                    <div className="book-shelf-changer">
+                                                    <select>
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                    </div>
+                                                </div>
+                                                <div className="book-title">{book.title}</div>
+                                                <div className="book-authors">{book.authors}</div>
+                                            </div>
+                                    </li>
+                                    ))}
+                                </ol>
+                            </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link 
+                        to='/search'
+                        >Add a book</Link>
+                </div>
+            </div>
+        )
     }
 }
+
+export default BookShelve
