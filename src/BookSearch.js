9841c2567a4f47eee49fadccb9b4164bbb138992
Dor Bar <dor4231@gmail.com>
2018-08-10 17:15:28 +0300

    feat: Display books for every change in input value

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -4,11 +4,27 @@ import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
 class BookSearch extends Component {
+    state = {
+        books: []
+    }
+
     searchInAPI = (query) => {
-        BooksAPI.search(query).then((res) => {
-            console.log(res)
-        })
-        
+        if (query === '') {
+            this.setState({ books: [] })
+        } else {
+            BooksAPI.search(query).then((books) => {
+                console.log(books);
+                if (books.error) {
+                    this.setState({ books: [] })
+                }else if (books) {
+                    this.setState({ books })
+                } else {
+                    this.setState({ books: [] })
+                }
+            }).catch(
+                this.setState({ books: []})
+            )
+        }
     }
 
     render() {
@@ -30,7 +46,30 @@ class BookSearch extends Component {
                 </div>
               </div>
               <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {this.state.books.map((book) => (
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select 
+                                            value={book.shelf}
+                                            onChange={(e) => {this.props.onSelectShelve(book, e.target.value)}}>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors}</div>
+                            </div>
+                        </li>
+                    ))}
+                </ol>
               </div>
             </div>
           )
