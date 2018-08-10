e26f138f48cefe5441aa6cfdce53909a80e75e5d
Dor Bar <dor4231@gmail.com>
2018-08-10 10:42:41 +0300

    feat: Create the BookSearch component and link  it back to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import BookShelve from './BookShelve'
+import BookSearch from './BookSearch'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -28,16 +29,7 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
-      books.map((book) => {
-        console.log(book);
-      })
-
-      console.log("Books:", this.state.books)
-    });
-    
-    BooksAPI.get("evuwdDLfAyYC").then(
-      (res) => console.log(res)
-    )
+    })
   }
 
   render() {
@@ -50,28 +42,7 @@ class BooksApp extends React.Component {
 
         <Route 
           path='/search'
-          render={() => (
-            <div className="search-books">
-              <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-                <div className="search-books-input-wrapper">
-                  {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-                  <input type="text" placeholder="Search by title or author"/>
-
-                </div>
-              </div>
-              <div className="search-books-results">
-                <ol className="books-grid"></ol>
-              </div>
-            </div>
-          )}/>
+          component={ BookSearch }/>
       </div>
     )
   }
